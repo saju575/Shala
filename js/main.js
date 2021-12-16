3d868fa949e08990ac5e67e7de53667a67a7360c
Shala <sakilahmedsaju575@gmail.com>
2021-12-16 21:48:32 +0600

    index.html,css,js edited

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+
+//preloader....
+
+$(window).on("load",function(){
+    $(".preloader").fadeOut("slow");
+});
+
 $(document).ready(function () {
     // ------------nav bar shrink---------
     $(window).on("scroll",function(){
@@ -126,5 +133,42 @@ $(document).ready(function () {
     $(".nav-link").on("click",function(){
         $(".navbar-collapse").collapse("hide");
     });
+
+    // -----------Toggle Theme - Light & dark-------
+    function toggleTheme(){
+        if(localStorage.getItem("shala-theme")!==null){
+            if(localStorage.getItem("shala-theme")==="dark"){
+                $("body").addClass("dark");
+            }
+            else{
+                $("body").removeClass("dark");
+            }
+        }
+        updateIcon();
+    }
+    toggleTheme();
+    $(".toggle-theme").on("click",function(){
+        $("body").toggleClass("dark");
+        if($("body").hasClass("dark")){
+            localStorage.setItem("shala-theme","dark");
+        }
+        else{
+            localStorage.setItem("shala-theme","light");
+        }
+        updateIcon();
+    })
+    function updateIcon(){
+        if($("body").hasClass("dark")){
+            $(".toggle-theme i").removeClass("fa-moon");
+            $(".toggle-theme i").addClass("fa-sun");
+
+        }
+        else{
+           
+            $(".toggle-theme i").removeClass("fa-sun");
+            $(".toggle-theme i").addClass("fa-moon");
+
+        }
+    }
     
 });
